df11fb4fda7264f182018724b5c270d565207f3a
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-10-29 19:59:44 -0400

    commit tree 1998af4b1103520a94e74e9ca27c0fff68fc90ea

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,22 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+    pick = nil 
+    array = hash.collect do |word, number|
+                number
+            end
+    value = array.first
+    array.each do |x|
+        if x < value
+            value = x
+        end
+    end
+    hash.each do |word, number|
+        if number == value
+            pick = word
+        end
+    end
+    pick
 end
+
